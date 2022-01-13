50c7febb04f758d8b6b41515f69ebf34882db0f0
Oliver <olive@DESKTOP-R044BGP>
2022-01-13 18:03:21 +0100

    new fac

--- test1.hs
+++ test1.hs
@@ -10,3 +10,21 @@ exists x (y:ys) = if x == y then True else exists x ys
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-2) + fib (n-1)
+
+fib' n = go 0 1 n
+go a b 0 = a
+go a b n = go b (a+b) (n-1)
+
+qsort []     = []
+qsort (x:xs) = let smaller = [e | e <- xs, e <= x]
+                   larger = [e | e <- xs, e > x]
+               in qsort smaller ++ [x] ++ qsort larger
+
+qsort' xs = if null xs then [] else qsort [e | e <- xs, e <= head xs] ++ [head xs] ++ [e | e <- xs, e > head xs]
+
+fac 1 = 1
+fac n = n * fac(n-1)
+
+fac' n = go' n 1
+go' 1 acc = acc
+go' n acc = go' (n-1) (acc*n)
